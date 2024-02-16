06b244b1a0bca0bbe6313cb09d048183901b3c39
Wafatriki <azdadwafa0@gmail.com>
2024-02-16 22:41:12 +0100

    Metodo remove

--- src/edp/Dic.java
+++ src/edp/Dic.java
@@ -1,5 +1,17 @@
 package edp;
 
 public class Dic extends Items {
+    private Items[] items;
+    private int size;
+
+    public void remove(String key) {
+        for(int i=0; i<size; i++) {
+            if(items[i].getKey() == key) {
+                items[i] = items[size - 1];
+                size--;
+            }
+        }
+    }
+    
     
 }
